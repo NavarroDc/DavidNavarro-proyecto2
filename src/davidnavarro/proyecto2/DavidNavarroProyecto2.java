a339cf9bb3b2a239d6464fd8f2ae6634a1b069cd
NavarroDc <130122601+NavarroDc@users.noreply.github.com>
2024-10-22 12:16:38 -0600

    version inicial

---
+++ src/davidnavarro/proyecto2/DavidNavarroProyecto2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package davidnavarro.proyecto2;
+
+/**
+ *
+ * @author angel
+ */
+public class DavidNavarroProyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
